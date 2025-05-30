2752cf64bafcf20cdbab25780e1c5800a71d9dc7
x.gaynazarov <xasang17@gmail.com>
2025-05-30 18:58:28 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="container pt-1">
+      <div class="card">
+        <h2>Актуальные новости {{ now }}</h2>
+        <h4>Открыто: {{ openRate }} | Прочитано: {{ readRate }}</h4>
+
+      </div>
+    </div>
+    <app-news
+        v-for="item in news"
+        :key="item.id"
+        :title="item.title"
+        :id="item.id"
+        :is-open="item.isOpen"
+        @open-news="openNews"
+        @read-news="readNews"
+    />
+  </div>
+
+</template>
+<script>
+import AppNews from './components/AppNews.vue'
+
+export default {
+  data() {
+    return {
+      openRate: 0,
+      readRate: 0,
+      now: new Date().toLocaleDateString(),
+      news: [
+        {
+          title: 'Новость 1',
+          id: 1,
+          isOpen: false
+        },
+        {
+          title: 'Новость 2',
+          id: 2,
+          isOpen: false
+        },
+        {
+          title: 'Новость 3',
+          id: 3,
+          isOpen: false
+        }
+      ]
+    }
+  },
+  methods: {
+    openNews() {
+      this.openRate++
+    },
+    readNews(){
+      this.readRate++
+    }
+  },
+  components: {
+    AppNews
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppNews.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card">
+    <h2>{{ title }}</h2>
+    <button class="btn" @click="open">
+      {{ isNewsOpen ? 'Закрыть' : 'Открыть' }}
+    </button>
+    <div v-if="isNewsOpen">
+      <hr />
+      <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, rem?</p>
+      <button
+          class="btn primary"
+          @click="noteRead"
+      >
+        Прочесть новость
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppNews',
+  props: {
+    title: String,
+    id: Number,
+    isOpen: Boolean
+  },
+  emits:['open-news', 'read-news'],
+  data() {
+    return {
+      isNewsOpen: this.isOpen
+    }
+  },
+  methods: {
+    open() {
+      this.isNewsOpen = !this.isNewsOpen
+      if (this.isNewsOpen) {
+        this.$emit('open-news')
+      }
+    },
+    noteRead(){
+      this.isNewsOpen = false
+      this.$emit('read-news')
+    }
+  }
+}
+</script>
